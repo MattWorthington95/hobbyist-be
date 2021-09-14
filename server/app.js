59d68b7f8af23502de229cff59d4d4ded5264eb0
amy <algrieve1@gmail.com>
2021-09-14 15:47:50 +0100

    add api router

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
+const apiRouter = require('./routes/api.router');
 
 dotenv.config({ path: 'server/config/config.env' });
 
@@ -9,5 +10,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
+app.use('/api', apiRouter);
 
 module.exports = app;
