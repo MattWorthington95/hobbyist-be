350745e7be9504a9a1ecd7824d84384a8e48c4dc
vikusja96 <vikusja96@gmail.com>
2021-09-15 14:53:30 +0100

    update db.js to adhere to eslint rules

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const client = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
     console.log(`MongoDB connected ${client.connection.host}`);
   } catch (err) {
