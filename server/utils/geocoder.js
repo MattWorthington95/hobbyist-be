aa2617ff0ce95fda4c315eb7b011bcdc7aa81431
amy <algrieve1@gmail.com>
2021-09-18 20:08:01 +0100

    create geocoder util function

--- server/utils/geocoder.js
+++ server/utils/geocoder.js
@@ -1,10 +1,34 @@
 const openGeocoder = require('node-open-geocoder');
 
-const geocoder = () => {
-  openGeocoder()
-    .geocode('24 linen court, manchester')
-    .end((err, res) => {
-      console.log(err, res);
-    });
+const geocoder = (document) => {
+  if (!document.location.formattedAddress) {
+    const addressStr = `${document.address.firstLine} ${document.address.postcode}`;
+    openGeocoder()
+      .geocode(addressStr)
+      .end((err, res) => {
+        if (err) console.log(err);
+        try {
+          if (!res[0]) {
+            document.location = {
+              type: 'Point',
+              coordinates: [],
+              formattedAddress: 'None'
+            };
+            document.save();
+          } else if (res[0].display_name) {
+            const { lat, lon, display_name } = res[0];
+            document.location = {
+              type: 'Point',
+              coordinates: [lat, lon],
+              formattedAddress: display_name
+            };
+            document.save();
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
+  }
 };
-geocoder();
+
+module.exports = geocoder;
