f2df412fad4cab7f531fb2fa66073cdfcb855b5e
amy <algrieve1@gmail.com>
2021-09-20 15:35:40 +0100

    set mongoose to use global promises

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  mongoose.Promise = global.Promise;
   try {
     const client = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
