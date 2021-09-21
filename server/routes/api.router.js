326980c28a1383198ac8044351bb033777736f3f
matt <mattworthington51@gmail.com>
2021-09-21 21:57:06 +0100

    added business user & user routes

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const apiRouter = express.Router();
 const clubsRouter = require('./clubs.router');
+const businessUserRouter = require('./businessUser.router');
+const userRouter = require('./user.router');
 
 apiRouter.get('/', (req, res) => {
   res.send('hello');
 });
 
 apiRouter.use('/clubs', clubsRouter);
+
+apiRouter.use('/businessuser', businessUserRouter);
+
+apiRouter.use('/user', userRouter);
 
 module.exports = apiRouter;
