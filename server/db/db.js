33016409e78cfedd7043b501ca0b36e2c067b696
vikusja96 <vikusja96@gmail.com>
2021-09-21 11:16:44 +0100

    add MongoMemoryServer package for testing

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server-core');
 
 const connectDB = async () => {
+  const mongod = await MongoMemoryServer.create();
   mongoose.Promise = global.Promise;
+
   try {
-    const client = await mongoose.connect(process.env.MONGO_URI, {
+    const uriStr = process.env.MONGO_URI ? process.env.MONGO_URI : mongod.getUri();
+    const client = await mongoose.connect(uriStr, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       dbName: process.env.MONGODB_NAME
     });
     console.log(`MongoDB connected ${client.connection.host}`);
+    return client;
   } catch (err) {
     console.log(err);
     process.exit(1);
