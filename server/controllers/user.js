9a652d3c3842c4b735759ad02cdc70524fb075fd
matt <mattworthington51@gmail.com>
2021-09-21 21:57:31 +0100

    added user controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 exports.postUserLogin = (req, res, next) => {
   const { username, password } = req.body;
   User.find({
-    $and: [{ username }, { password }],
+    $and: [{ username }, { password }]
   }).then((user) => {
     if (user.length < 1) {
       res.status(404).send({ msg: 'incorrect username or password ' });
