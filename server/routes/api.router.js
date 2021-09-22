520b4df6f8e95ed5acddec80413b1dcbdbb372b4
vikusja96 <vikusja96@gmail.com>
2021-09-22 10:51:56 +0100

    add extra routes

--- server/routes/api.router.js
+++ server/routes/api.router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const apiRouter = express.Router();
 const clubsRouter = require('./clubs.router');
+const businessUserRouter = require('./businessUser.router');
+const userRouter = require('./user.router');
 
 apiRouter.get('/', (req, res) => {
   res.send('hello');
 });
 
 apiRouter.use('/clubs', clubsRouter);
+apiRouter.use('/businessUsers', businessUserRouter);
+apiRouter.use('/users', userRouter);
+
 
 module.exports = apiRouter;
