b7395f867054bb6f136714fa2d307b3d604b3cce
amy <algrieve1@gmail.com>
2021-09-22 18:58:47 +0100

    convert geocoder into returning a promise

--- server/utils/geocoder.js
+++ server/utils/geocoder.js
@@ -1,34 +1,44 @@
 const openGeocoder = require('node-open-geocoder');
 
 const geocoder = (document) => {
-  if (!document.location.formattedAddress) {
-    const addressStr = `${document.address.firstLine} ${document.address.postcode}`;
-    openGeocoder()
-      .geocode(addressStr)
-      .end((err, res) => {
-        if (err) console.log(err);
-        try {
-          if (!res[0]) {
-            document.location = {
-              type: 'Point',
-              coordinates: [],
-              formattedAddress: 'None'
-            };
-            document.save({ suppressWarning: true });
-          } else if (res[0].display_name) {
-            const { lat, lon, display_name } = res[0];
-            document.location = {
-              type: 'Point',
-              coordinates: [lat, lon],
-              formattedAddress: display_name
-            };
-            document.save({ suppressWarning: true });
+  return new Promise((resolve, reject) => {
+    if (!document.location.formattedAddress) {
+      const addressStr = `${document.address.firstLine} ${document.address.postcode}`;
+
+      openGeocoder()
+        .geocode(addressStr)
+        .end((err, res) => {
+          if (err) reject(err);
+          try {
+            if (!res[0]) {
+              document.location = {
+                type: 'Point',
+                coordinates: [],
+                formattedAddress: 'None'
+              };
+
+              resolve(document.save({ suppressWarning: true }));
+            } else if (res[0].display_name) {
+              const { lat, lon, display_name } = res[0];
+
+              document.location = {
+                type: 'Point',
+                coordinates: [lat, lon],
+                formattedAddress: display_name
+              };
+
+              resolve(document.save({ suppressWarning: true }));
+            } else {
+              resolve();
+            }
+          } catch (error) {
+            reject(error);
           }
-        } catch (error) {
-          console.log(error);
-        }
-      });
-  }
+        });
+    } else {
+      resolve();
+    }
+  });
 };
 
 module.exports = geocoder;
